ce0615c315cf49f2e318b4fa41e9ef6414abb269
Admin <ekaterina28041998@gmail.com>
2025-02-18 03:59:00 +0300

    Ветка мастер. Строки. Дз

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        String task = "Task-";
+        System.out.println(task + 1);
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName;
+
+        fullName = firstName + " " + middleName + " " + lastName;
+
+                System.out.println("Ф. И. О. сотрудника - " + fullName);
+
+        System.out.println(task + 2);
+        System.out.printf("Данные Ф. И. О. сотрудника для заполнения отчета - %s%n", fullName.toUpperCase());
+
+        System.out.println(task + 3);
+
+        fullName = "Иванов Семён Семёнович";
+        String corretedFullName = fullName.replace("ё", "е")
+        System.out.printf("Данные Ф. И. О. сотрудника - %s%n", fullName);
     }
 }
